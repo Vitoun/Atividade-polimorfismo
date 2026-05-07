c2de5ff92e7abb0550a78f84e07fbcae3f25d245
Vitor-shift-it <vd347237@gmail.com>
2026-05-06 22:34:24 -0300

    Add files via upload

---
+++ Animal/src/animal/AnimalTeste.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package animal;
+
+/**
+ *
+ * @author Admin
+ */
+public class AnimalTeste {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Animal a1 = new Cachorro();
+        Animal a2 = new Gato();
+        
+        a1.fazerSom();
+        a2.fazerSom();
+    }
+    
+}
---
+++ Animal/src/animal/Gato.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package animal;
+
+/**
+ *
+ * @author Admin
+ */
+public class Gato extends Animal {
+    
+    void fazerSom(){
+        System.out.println("Miau");
+    }
+    
+}
