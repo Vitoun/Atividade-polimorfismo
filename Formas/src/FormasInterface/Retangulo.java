9abe510b8ca5162dc2070392abc6bf37f292c034
Vitoun <vd347237@gmail.com>
2026-05-07 22:43:58 -0300

    Add files via upload

---
+++ Formas/src/FormasInterface/Retangulo.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package FormasInterface;
+
+/**
+ *
+ * @author Admin
+ */
+abstract class Retangulo extends formasinterface{
+    
+    @Override
+    System.out.println("");
+}
