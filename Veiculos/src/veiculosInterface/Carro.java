3df210ed18746a3f7719d42d22eb32f96a3c0836
Vitoun <vd347237@gmail.com>
2026-05-11 22:40:23 -0300

    Add files via upload

---
+++ Veiculos/src/veiculosInterface/Carro.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package veiculosInterface;
+
+/**
+ *
+ * @author Admin
+ */
+public class Carro implements Veiculo{
+    
+    public void mover(){
+        
+       @Override
+       System.out.println("O Carro esta se movendo");
+    }
+    
+    
+}
