6ea39063b8cccf50d4aee5d837f12567561f0bff
Vitoun <vd347237@gmail.com>
2026-05-11 22:02:49 -0300

    Add files via upload

---
+++ Animal/src/animalinterface/Cachorro.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package animalinterface;
+
+/**
+ *
+ * @author Admin
+ */
+public class Cachorro implements Animal {
+    
+    public void fazerSom(){
+     System.out.println("Au au");   
+    }
+    
+    
+}
---
+++ Animal/src/animalinterface/Gato.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package animalinterface;
+
+/**
+ *
+ * @author Admin
+ */
+public class Gato implements Animal {
+    
+    
+    public void fazerSom(){
+     System.out.println("Miau");   
+    }
+}
